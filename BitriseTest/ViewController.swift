7d2a41c8c4e2e592e2d89c4eab88923b4fc19945
Claudio Carvalho <claudiocarvalhodev@gmail.com>
2020-03-26 18:17:06 -0700

    Initial Commit

---
+++ BitriseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseTest
+//
+//  Created by Claudio Carvalho on 26/03/20.
+//  Copyright © 2020 Claudio Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
